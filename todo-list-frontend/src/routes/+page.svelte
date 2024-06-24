a7d9f21caf553548f31963b5f65169196d4d6cff
Nikki <21485015@student.curtin.edu.au>
2024-06-25 00:53:12 +0800

    Add existing code to repository

--- todo-list-frontend/src/routes/+page.svelte
+++ todo-list-frontend/src/routes/+page.svelte
@@ -1,62 +1,72 @@
 <script lang="ts">
-import { onMount } from "svelte";
+    //Define the todo type
+    type Todo = {
+        id: number;
+        name: string;
+        isChecked: boolean;
+    };
 
-let pingResponseMessage: string;
-let pingResponseCount: number;
+    //initialize the todos array
+    let todos: Todo[] = [];
+    //string for new todo input text 
+    let newTodoText: string = '';
 
-let todos: TodoItem[] = [];
-let newTodoDescription = "";
+    //fetch todoes from the backend
+    async function getTodos() {
+        const todosData = await fetch("https://localhost:5000");
 
-async function getTodos() {
-	const response = await fetch("http://localhost:3000/todo-item");
-	const responseJson = await response.json();
-	console.log(todos);
-	todos = responseJson;
-}
+        const todos = await todosData.json(); 
+        return todos;
+    }
 
-async function addTodoButtonclick() {
-	const createTodoItemRequest: CreateTodoItemRequest = {
-		description: newTodoDescription,
-	};
-	const createTodoItemRequestJson = JSON.stringify(createTodoItemRequest);
+    // function onGetTodoButtonClick() {
+    //     getTodos();
+    // }
+    
+    //add new todo
+    function createTodos() {
+        if (newTodoText.trim() != '') {
+            const newTodo: Todo = {
+                id: Date.now(), //date is just basic storage
+                name: newTodoText,
+                isChecked: false
+            };
+            todos = [...todos, newTodo];
+            newTodoText = '';
+        }
+    }
 
-	const response = await fetch("http://localhost:3000/todo-item", {
-		method: "POST", // This is a POST request, not the default GET
-		headers: {
-			"Content-Type": "application/json", // We are sending JSON
-			mode: "no-cors", // no-cors, *cors, same-origin
-		},
-		body: createTodoItemRequestJson, // The JSON we are sending
-	});
 
-	console.log(response);
+    function deleteTodo() {
+        function deleteTodo(id: number) {
+            todos = todos.filter(todo => todos.id !== id);
+        }
+    }
 
-	// Once the new todo is added, re-ask server for to-do items (NOTE: This is flawed in a number of ways. Why?)
-	getTodos();
-}
-
-// This runs when the page is loaded into the browser
-onMount(() => {
-	getTodos();
-});
+    function markTodoAsDone(id: number) {
+        todos = todos.map(todo =>
+            todo.id === id ? {...todo, done: !todo.done } : todo
+        );
+    }
 </script>
 
-<h1>POGGERS To-do list</h1>
-
-<label for="new-todo-description">Description</label>
-<input name="new-todo-description" bind:value={newTodoDescription}/>
-<button on:click={addTodoButtonclick}>Add new todo</button>
-
 {#each todos as todo}
-	<div class="todo-item">[{todo.isDone ? "X" : " "}] {todo.description}</div>
+    <p>- {todo}</p>
 {/each}
 
+<h1>My To-do list</h1>
+<!-- Input for new todo -->
+<div> 
+    <input type="text" bind:value={newTodoText} placeholder="New todo">
+    <button on:click={createTodos}>Add Todo</button>
+</div>
 
-<style>
-	.todo-item {
-		background-color: cornflowerblue;
-		width: 200px;
-		margin: 5px;
-		padding: 10px;
-	}
-</style>
+<!-- Display the todo list -->
+ <!-- {each todos as todo(todo.id)}
+ {
+    <button on:click={() => deleteTodo(todo.id)}>Delete</button>button>
+    <input type="checkbox" bind:checked={todo.isChecked}>
+ } -->
+<div>
+<button on:click={createTodos}>add Todo</button>
+</div>
